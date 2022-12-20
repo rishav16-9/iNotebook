a0a489348eeb49bec69a7df3bf6af42a4da05684
Rishav <suranarishav08@gmail.com>
2022-12-20 14:16:01 +0530

    Backend complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,12 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'Harryisagoodb$boy'
-// Create a User using: Post "/api/auth/CreateUser". No login required
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+
+const JWT_SECRET = 'Harryisagoodb$oy'
+
+// Route 1: Create a User using: Post "/api/auth/CreateUser". No login required
 
 router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -41,13 +44,70 @@ router.post('/createuser', [
       }
     }
     const authtoken = jwt.sign(data, JWT_SECRET)
-    res.json({authtoken})
+    res.json({ authtoken })
     // res.json({ user })
 
   } catch (error) {
     console.log(error.messgae);
-    res.status(500).send("Some error occured")
+    res.status(500).send("Internal Seerver Error")
   }
 })
+
+
+// Route 2: Authenticate a user using: Post "/api/auth/CreateUser". No login required
+
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password Cannot be blank').exists(),
+
+], async (req, res) => {
+  // If there are error return bad request and the errors
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ "error": "Please try to login with correct credential" })
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password)
+    if (!passwordCompare) {
+      return res.status(400).json({ "error": "Please try to login with correct credential" })
+    }
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET)
+    res.json({ authtoken })
+
+  } catch (error) {
+    console.log(error.messgae);
+    res.status(500).send("Internal Seerver Error")
+  }
+}
+)
+
+
+// Route 3: Get loggedin User Details using : Post "api/auth/getuser". Login required
+router.post('/getuser', fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id
+    const user = await User.findById(userId).select("-password")
+    res.send(user)
+  } catch (error) {
+    console.log('sdfghjkl');
+    console.log(error.message);
+    res.status(500).send("Internal Seerver Error")
+  }
+}
+)
+
 
 module.exports = router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,89 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note')
 
-router.get('/', (req, res) => {
-    res.json([])
+// Route 1: Get all the notes using get"api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.log(error.messgae);
+        res.status(500).send("Internal Seerver Error")
+    }
+
+})
+
+// Route2: Add a new notes using post "api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be minimum of 5 character').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body
+        // If there are error return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savednote = await note.save()
+        res.json(savednote)
+    } catch (error) {
+        console.log(error.messgae);
+        res.status(500).send("Internal Seerver Error")
+    }
+})
+
+
+// Route3: Update an existing notes notes using put "api/notes/updatenote". Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body
+    //Creeate a new note object
+    try {
+        const newNote = {};
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag }
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note })
+    } catch (error) {
+        console.log(error.messgae);
+        res.status(500).send("Internal Seerver Error")
+    }
+})
+
+// Route4: Delete an existing notes notes using post "api/notes/deletenote". Login required
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    // Find the note to be delete and delete it
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+        // Allow deletion only if user owns the note
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted", note: note })
+    } catch (error) {
+        console.log(error.messgae);
+        res.status(500).send("Internal Seerver Error")
+    }
 })
 
 module.exports = router
